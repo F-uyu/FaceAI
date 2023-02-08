a2a38d90926f4a5b476b75cac9b6f06c58820c69
Matthew Yu <matthewyu@Matthews-MacBook-Pro.local>
2023-02-07 17:45:51 -0800

    Fixed eyes

--- src/main.py
+++ src/main.py
@@ -12,22 +12,21 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face.detectMultiScale(
         gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
+        scaleFactor=1.05,
+        minNeighbors=7,
+        minSize=(40, 40),
         flags =cv2.CASCADE_SCALE_IMAGE
     )
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
         eyes = eye.detectMultiScale(
-            gray[y:y+h, x:x+w],
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags =cv2.CASCADE_SCALE_IMAGE
+            roi_gray
         )
+        
         for (lx, ly, lw, lh) in eyes:
-            cv2.rectangle(frame, (lx, ly), (lx+lw, ly+lh), (0, 255, 0), 2)
+            cv2.rectangle(roi_color, (lx, ly), (lx+lw, ly+lh), (0, 255, 0), 2)
 
     cv2.imshow("Faces", frame)
     if cv2.waitKey(1) == ord('q'):
